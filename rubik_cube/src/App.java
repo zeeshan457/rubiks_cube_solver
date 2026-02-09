b0da83c2478a6ffe305ee88ece65fc08d3fd585a
zeeshan <zeeshanm2405@gmail.com>
2026-02-09 23:42:20 +0000

    project commit
    just created a java project and added some files.

---
+++ rubik_cube/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello, World! " + i);
+
+        }
+
+    }
+}
